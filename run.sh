bee757b118642fdfc6b6c4fc5064db674544a923
asatterlee <asatterlee@malwarebytes.com>
2020-03-03 13:07:06 -0500

    added charts and d3 libraries

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-docker build -t react-docker:dev .
+docker build -t react-charts:dev .
 
-docker run -v ${PWD}:/app -v /app/node_modules -p 3000:3000 --rm react-docker:dev
+docker run -v ${PWD}:/app -v /app/node_modules -p 3000:3000 --rm react-charts:dev
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheets/index.css';
-import App from './components/App';
+import App from './containers/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
